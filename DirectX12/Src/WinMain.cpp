4b9e4ad583051832c6b779680b7a52101f59daec
yunosuke matsumoto <yunosukematsumoto.hal@gmail.com>
2018-12-05 16:20:26 +0900

    WinMain関数を定義

---
+++ DirectX12/Src/WinMain.cpp
@@ -0,0 +1,19 @@
+#include "Main.h"
+#include <crtdbg.h>
+
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment( lib, "winmm.lib" )
+#pragma comment(lib,"xinput.lib")
+//#pragma comment(lib,"assimp-vc140-mt.lib")
+#pragma comment( lib, "legacy_stdio_definitions.lib" )
+
+
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+
+	return 0;
+}

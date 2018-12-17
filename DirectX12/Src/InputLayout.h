960643bd2f4df3cd1b7f050ab1391405c7db9f49
yunosuke matsumoto <yunosukematsumoto.hal@gmail.com>
2018-12-17 17:03:05 +0900

    旧ライブラリで作成していたPSOクラスをそのまま移してきた

---
+++ DirectX12/Src/InputLayout.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <vector>
+#include "DirectX12.h"
+
+class cInputLayout
+{
+public:
+	cInputLayout() = default;
+	~cInputLayout() = default;
+
+	template <typename T>		// シェーダ側で使用する型と同義のMath型を渡すとその分のアライメントを計算する
+	void AddElement(const char* SemanticName, DXGI_FORMAT Format, D3D12_INPUT_CLASSIFICATION InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, UINT SemanticIndex = 0, UINT InputSlot = 0, UINT InstanceDataStepRate = 0);
+	inline D3D12_INPUT_ELEMENT_DESC* GetInputElementDesc() { return &m_InputLayout[0]; };
+	inline size_t GetSize() { return m_InputLayout.size(); };
+private:
+	std::vector<D3D12_INPUT_ELEMENT_DESC> m_InputLayout;
+	UINT m_AlignedByteOffset[15] = { 0 };
+};
+
+template<typename T>
+inline void cInputLayout::AddElement(const char* SemanticName, DXGI_FORMAT Format, D3D12_INPUT_CLASSIFICATION InputSlotClass, UINT SemanticIndex, UINT InputSlot, UINT InstanceDataStepRate)
+{
+	m_InputLayout.push_back({ SemanticName, SemanticIndex, Format, InputSlot, m_AlignedByteOffset[InputSlot], InputSlotClass, InstanceDataStepRate });
+	m_AlignedByteOffset[InputSlot] += sizeof(T);		// アライメントサイズを増加する
+}
---
+++ DirectX12/Src/PipelineStateObj.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Main.h"
+
+class cInputLayout;
+class cShaderByte;
+
+class cPipelineStateObj
+{
+public:
+	cPipelineStateObj();
+	~cPipelineStateObj() = default;
+	void CreatePipelineState();
+
+	//=====以下の関数は設定必須======
+	void RenderTargetSetting(DXGI_FORMAT* format, unsigned RTNum);
+	void ShaderBytecodeSetting(ID3D10Blob* vs, ID3D10Blob* ps, ID3D10Blob* ds = nullptr, ID3D10Blob* hs = nullptr, ID3D10Blob* gs = nullptr);
+	void ShaderBytecodeSetting(cShaderByte* pShaderData);
+	void RootSignatureSetting(ID3D12RootSignature* rs);
+	void InputLayoutSetting(D3D12_INPUT_ELEMENT_DESC* inputLayout, unsigned num);
+	void InputLayoutSetting(cInputLayout* inputLayout);
+	// ==========================
+
+	// 他の設定項目への対応は暫定的にオブジェクト自体を渡して行う
+	inline D3D12_GRAPHICS_PIPELINE_STATE_DESC* GetPipelineDESC() { return &psoDesc; };
+	inline Microsoft::WRL::ComPtr<ID3D12PipelineState> GetPipelineState() { return m_Pso; };
+private:
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> m_Pso;
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc = {};
+};

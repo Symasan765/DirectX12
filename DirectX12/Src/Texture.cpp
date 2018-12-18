8b8ceb34ef00787328a56b206cc5d7c67774bb22
yunosuke matsumoto <yunosukematsumoto.hal@gmail.com>
2018-12-18 17:30:19 +0900

    テクスチャを扱うクラスを定義

---
+++ DirectX12/Src/Texture.cpp
@@ -0,0 +1,18 @@
+#include "Texture.h"
+#include "Utility.h"
+#include "DirectX12.h"
+
+cTexture::cTexture(D3D12_DESCRIPTOR_HEAP_FLAGS flag)
+{
+	m_DescriptorHeap = std::make_unique<cDescriptorBase>(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, flag,1);
+}
+
+Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> cTexture::GetDescriptorHeap()
+{
+	return m_DescriptorHeap->GetHeap();
+}
+
+void cTexture::SetFilePath(std::string path)
+{
+	m_FilePath = path;
+}
---
+++ DirectX12/Src/Texture.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <wrl/client.h>
+#include <d3d12.h>
+#include <string>
+#include "DescriptorBase.h"
+#include <memory>
+
+/// <summary>
+/// ひとつのテクスチャを保持するクラス
+/// </summary>
+class cTexture {
+public:
+	cTexture(D3D12_DESCRIPTOR_HEAP_FLAGS flag = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE);			// flag変数はどこのシェーダから読めるようにするか
+	~cTexture() = default;
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> GetDescriptorHeap();
+	inline Microsoft::WRL::ComPtr<ID3D12Resource> GetTextureResource() { return m_pTex; };
+	inline std::string GetPath() { return m_FilePath; };
+	void SetFilePath(std::string path);
+private:
+	Microsoft::WRL::ComPtr<ID3D12Resource> m_pTex;		// テクスチャリソース
+	std::unique_ptr<cDescriptorBase> m_DescriptorHeap;
+	std::string m_FilePath;
+};

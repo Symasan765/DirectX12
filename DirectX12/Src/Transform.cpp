7ebacdd8c74459cfdd7d1e1a3471a583cd0a4e46
yunosuke matsumoto <yunosukematsumoto.hal@gmail.com>
2018-12-22 18:21:43 +0900

    トランスフォームにワールド行列計算機能を追加

--- DirectX12/Src/Transform.cpp
+++ DirectX12/Src/Transform.cpp
@@ -1,6 +1,34 @@
 #include "Transform.h"
+#include "GameTime.h"
+using namespace DirectX;
 
 cTransform::cTransform(cBehavior * owner) : cComponentBase(owner)
 {
 
+}
+
+DirectX::XMFLOAT4X4 cTransform::ToWorldMatrix(int frameIndex)
+{
+	XMMATRIX mat = XMMatrixScaling(m_Scale[frameIndex].x, m_Scale[frameIndex].y, m_Scale[frameIndex].z);
+	mat *= XMMatrixRotationRollPitchYaw(m_Rotation[frameIndex].x, m_Rotation[frameIndex].y, m_Rotation[frameIndex].z);
+	mat *= XMMatrixTranslation(m_Position[frameIndex].x, m_Position[frameIndex].y, m_Position[frameIndex].z);
+
+	XMFLOAT4X4 ret;
+	XMStoreFloat4x4(&ret, mat);
+	return ret;
+}
+
+DirectX::XMFLOAT4 & cTransform::Position()
+{
+	return m_Position[cGameTime::FrameIndex()];
+}
+
+DirectX::XMFLOAT4 & cTransform::Rotation()
+{
+	return m_Rotation[cGameTime::FrameIndex()];
+}
+
+DirectX::XMFLOAT4 & cTransform::Scale()
+{
+	return m_Scale[cGameTime::FrameIndex()];
 }
--- DirectX12/Src/Transform.h
+++ DirectX12/Src/Transform.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "ComponentBase.h"
 #include "DXMath.h"
+#include "Render.h"
 
 /// <summary>
 /// オブジェクトの位置や回転、サイズなどを管理する
@@ -14,7 +15,14 @@ public:
 	void Update(float deltaTime) {};
 	void Start() {};
 
-	DirectX::XMFLOAT4 m_Position;
-	DirectX::XMFLOAT4 m_Rotation;
-	DirectX::XMFLOAT4 m_Scale;
+	DirectX::XMFLOAT4X4 ToWorldMatrix(int frameIndex);
+	DirectX::XMFLOAT4& Position();
+	DirectX::XMFLOAT4& Rotation();
+	DirectX::XMFLOAT4& Scale();
+
+private:
+	// 描画と更新を並列で行うのでダブルバッファ化
+	DirectX::XMFLOAT4 m_Position[Render::g_LatencyNum];
+	DirectX::XMFLOAT4 m_Rotation[Render::g_LatencyNum];
+	DirectX::XMFLOAT4 m_Scale[Render::g_LatencyNum];
 };
